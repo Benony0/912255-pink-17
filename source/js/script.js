116702b009de1c8dca0fa2d49cadc2d0e85bf635
GanievStas <benony0gmail.com>
2019-08-30 06:18:37 +1000

    Попапы готовы

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,10 @@
 var siteNav = document.querySelector(".header__container");
 var siteNavBtn = document.querySelector(".header__button-menu");
 
+var link = document.querySelector(".btn-form-submit");
+var popup = document.querySelector(".popup__send");
+var close = document.querySelector(".btn-popup-send-ok");
+
 siteNav.classList.remove("header__container_nojs");
 
 siteNavBtn.addEventListener("click", function() {
@@ -11,4 +15,16 @@ siteNavBtn.addEventListener("click", function() {
     siteNav.classList.add("header__container_closed");
     siteNav.classList.remove("header__container_opened");
   }
+});
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("hidden");
+  // popup.classList.add("display-show");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("hidden");
+  // popup.classList.remove("display-show");
 });
